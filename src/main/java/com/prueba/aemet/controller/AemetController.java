af50943effa7f73d18f84ce6e7df4767df21a659
Juan Martinez <juan.martinez@metaenlace.com>
2023-07-14 07:57:54 +0200

    Fixed imports

--- src/main/java/com/prueba/aemet/controller/AemetController.java
+++ src/main/java/com/prueba/aemet/controller/AemetController.java
@@ -1,10 +1,8 @@
 package com.prueba.aemet.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.prueba.aemet.modelDTO.MunicipioDTO;
 import com.prueba.aemet.modelDTO.PrediccionMunDiaSigDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
